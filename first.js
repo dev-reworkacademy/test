0c81c00b580b4d203b31424d3e5d27ff9bc3eb2c
dev-reworkacademy <132952009+dev-reworkacademy@users.noreply.github.com>
2025-07-15 08:32:45 +0100

    Update first.js

--- first.js
+++ first.js
@@ -1,3 +1,10 @@
 // // b.js
 // const loader = global[Symbol.for('loader')];
 // module.exports = loader(__filename);
+
+// c.js
+module.exports = {
+  run: () => {
+    console.log("🔥 Running secret logic from c.js");
+  }
+};
